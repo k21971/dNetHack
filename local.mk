1f1e1bdf500a7968f91956216cd93e8d63459189
k21971 <keith.simpson1971@gmail.com>
2021-05-30 15:17:28 -0400

    Hardfought server-specific files.

---
+++ local.mk
@@ -0,0 +1,8 @@
+GAMEDIR = /opt/nethack/chroot/dnethack-3.21.0
+CFLAGS = -g3 -O0 -Wno-format-overflow
+CPPFLAGS = -DWIZARD=\"build\" -DCOMPRESS=\"/bin/gzip\" -DCOMPRESS_EXTENSION=\".gz\"
+CPPFLAGS += -DHACKDIR=\"/dnethack-3.21.0\" -DDUMPMSGS=200
+CPPFLAGS += -DDUMP_FN=\"/dgldir/userdata/%N/%n/dnethack/dumplog/%t.dnh.txt\"
+CPPFLAGS += -DHUPLIST_FN=\"/dgldir/userdata/%N/%n/dnethack/hanguplist.txt\"
+CPPFLAGS += -DEXTRAINFO_FN=\"/dgldir/extrainfo-dnh/%n.extrainfo\"
+
