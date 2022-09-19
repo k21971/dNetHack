76f6a8918cbd84e61fb0c8a0cc50be614d384d65
k21971 <keith.simpson1971@gmail.com>
2022-09-19 00:36:21 +0000

    Update install scripts/file for new version (3.21.4).

--- install-to-chroot.sh
+++ install-to-chroot.sh
@@ -17,7 +17,7 @@ USRGRP="games:games"
 NH_GIT="/home/build/dNetHack"
 # HACKDIR from include/config.h; aka nethack subdir inside chroot
 # Make a new one each time save compat is broken
-NHSUBDIR="dnethack-3.21.3"
+NHSUBDIR="dnethack-3.21.4"
 #for combining xlogfile, etc on minor version bump
 # END OF CONFIG
 ##############################################################################
@@ -43,8 +43,8 @@ set -e
 umask 022
 
 echo "Creating inprogress and userdata directories"
-mkdir -p "$NAO_CHROOT/dgldir/inprogress-dnh3213"
-chown "$USRGRP" "$NAO_CHROOT/dgldir/inprogress-dnh3213"
+mkdir -p "$NAO_CHROOT/dgldir/inprogress-dnh3214"
+chown "$USRGRP" "$NAO_CHROOT/dgldir/inprogress-dnh3214"
 mkdir -p "$NAO_CHROOT/dgldir/extrainfo-dnh"
 chown "$USRGRP" "$NAO_CHROOT/dgldir/extrainfo-dnh"
 
