9dd4d614858e5995c03535709b29a83a877b5495
k21971 <keith.simpson1971@gmail.com>
2024-09-07 21:23:32 +0000

    Added some commented out lines for when I want to install this locally.

--- local.mk
+++ local.mk
@@ -1,7 +1,9 @@
 GAMEDIR = /opt/nethack/chroot/dnethack-3.23.0
+#GAMEDIR = /usr/games/lib/dnethackdir
 CFLAGS = -g3 -O0 -Wno-format-overflow
 CPPFLAGS = -DWIZARD=\"build\" -DCOMPRESS=\"/bin/gzip\" -DCOMPRESS_EXTENSION=\".gz\"
 CPPFLAGS += -DHACKDIR=\"/dnethack-3.23.0\" -DDUMPMSGS=100
+#CPPFLAGS += -DHACKDIR=\"/usr/games/lib/dnethackdir\" -DDUMPMSGS=100
 CPPFLAGS += -DDUMP_FN=\"/dgldir/userdata/%N/%n/dnethack/dumplog/%t.dnh.txt\"
 CPPFLAGS += -DHUPLIST_FN=\"/dgldir/userdata/%N/%n/dnethack/hanguplist.txt\"
 CPPFLAGS += -DEXTRAINFO_FN=\"/dgldir/extrainfo-dnh/%n.extrainfo\"
