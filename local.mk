49dc0a868fb6e4c351783a9e34a7e9b392a5f118
k21971 <keith.simpson1971@gmail.com>
2025-05-16 16:33:10 +0000

    Update install scripts to support new version (3.24.0).

--- local.mk
+++ local.mk
@@ -1,8 +1,8 @@
-GAMEDIR = /opt/nethack/chroot/dnethack-3.23.0
+GAMEDIR = /opt/nethack/chroot/dnethack-3.24.0
 #GAMEDIR = /usr/games/lib/dnethackdir
 CFLAGS = -g3 -O0 -Wno-format-overflow
 CPPFLAGS = -DWIZARD=\"build\" -DCOMPRESS=\"/bin/gzip\" -DCOMPRESS_EXTENSION=\".gz\"
-CPPFLAGS += -DHACKDIR=\"/dnethack-3.23.0\" -DDUMPMSGS=100
+CPPFLAGS += -DHACKDIR=\"/dnethack-3.24.0\" -DDUMPMSGS=100
 #CPPFLAGS += -DHACKDIR=\"/usr/games/lib/dnethackdir\" -DDUMPMSGS=100
 CPPFLAGS += -DDUMP_FN=\"/dgldir/userdata/%N/%n/dnethack/dumplog/%t.dnh.txt\"
 CPPFLAGS += -DHUPLIST_FN=\"/dgldir/userdata/%N/%n/dnethack/hanguplist.txt\"
